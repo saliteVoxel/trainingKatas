81c8cd4ef7664e7a88784f084c6113903c6fad01
salite <salite@voxelgroup.net>
2021-11-30 17:20:08 +0100

    feat: BeDividedByOneHundredButNotByFourHundred

--- LeapYear/LeapYear/LeapYearCalculator.cs
+++ LeapYear/LeapYear/LeapYearCalculator.cs
@@ -6,8 +6,11 @@ namespace LeapYear
     {
         public static bool Calculate(int number)
         {
+            if (number % 100 == 0 && number % 400 != 0)
+            {
+                return false;
+            }
             return number % 4 == 0;
-
         }
     }
 }
